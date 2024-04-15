0900d15de796565b0163b3243579fde08316952f
Crowdbotics <team@crowdbotics.com>
2024-04-15 08:55:59 +0000

    Initial commit

---
+++ backend/wild_base_47505/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for wild_base_47505 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "wild_base_47505.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wild-base-47505",
+  url: "https://wild-base-47505.botics.co",
+  api: "https://wild-base-47505.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
